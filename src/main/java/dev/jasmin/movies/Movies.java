03658a154a79a5e52e867ab7e231e45301c2937c
jasmin <jajalorino@gmail.com>
2025-07-13 19:13:25 +0800

    added lombok annotations

--- src/main/java/dev/jasmin/movies/Movies.java
+++ src/main/java/dev/jasmin/movies/Movies.java
@@ -1,12 +1,19 @@
 package dev.jasmin.movies;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import java.util.List;
 
 @Document(collection="movies")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Movies {
-
+    @Id
     private ObjectId id;
     private String imdbId;
     private String title;
