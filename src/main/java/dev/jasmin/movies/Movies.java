f16bf7675ea73c7f1a2b86d96ee7342a89121abc
jasmin <jajalorino@gmail.com>
2025-07-13 19:33:17 +0800

    added interface (movie rep)

--- src/main/java/dev/jasmin/movies/Movies.java
+++ src/main/java/dev/jasmin/movies/Movies.java
@@ -6,6 +6,8 @@ import lombok.NoArgsConstructor;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.DocumentReference;
+
 import java.util.List;
 
 @Document(collection="movies")
@@ -22,4 +24,7 @@ public class Movies {
     private String poster;
     private List<String> genres;
     private List<String> backdrops;
+
+    @DocumentReference
+    private List<Reviews> reviewIds;
 }
